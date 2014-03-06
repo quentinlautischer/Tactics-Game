f4fd4b1960dabbd9c41e9ad58f56da901ef96ae2
joshuajjwhite <jjwhite@ualberta.ca>
2014-03-06 10:04:52 -0700

    probability cocunter

--- analyze.py
+++ analyze.py
@@ -1,2 +1,20 @@
 def destroy_prob(attacker, defender, current_tile, turns):
-    return [867.5309]
+
+	crit_prob = {-1:20 , 0:50, 1:20, 2:10}
+	prob = []    
+	d = attacker.get_damage(defender,current_tile)
+	hp = defender.health
+
+	for k in range(len(turns) + 1):
+		temp = 0
+		for i in crit_prob:
+			if (i + d) >= hp:
+				temp += crit_prob[i]
+
+		hp -= d
+		prob[k] = temp
+
+
+
+
+    return prob[0]

4fff9fc3cbbd7aa5864f9d6185dee22cf9db49c2
joshuajjwhite <jjwhite@ualberta.ca>
2014-03-06 10:34:43 -0700

    tryin to fix this

--- analyze.py
+++ analyze.py
@@ -1,20 +1,19 @@
 def destroy_prob(attacker, defender, current_tile, turns):
 
-	crit_prob = {-1:20 , 0:50, 1:20, 2:10}
+	crit_prob = {-1:.2 , 0:.5, 1:.2, 2:.1}
 	prob = []    
 	d = attacker.get_damage(defender,current_tile)
 	hp = defender.health
 
-	for k in range(len(turns) + 1):
+	for k in range(turns + 1):
 		temp = 0
 		for i in crit_prob:
 			if (i + d) >= hp:
 				temp += crit_prob[i]
 
 		hp -= d
-		prob[k] = temp
+		prob.append(temp)
 
 
 
-
-    return prob[0]
+	return prob
